668f68dc1a6eeec6fa6072670300d3e420c80da2
Javier sanchez <javier.ant.sanchezc@gmail.com>
2020-12-03 09:56:35 -0500

    creacion de controller

---
+++ src/main/java/com/app/bar/appbar/controller/productoController.java
@@ -0,0 +1,18 @@
+package com.app.bar.appbar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class productoController {
+
+    @GetMapping("/productos")
+    public String Consulta(){
+
+        return null;
+    }
+
+
+}
