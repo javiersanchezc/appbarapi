05c6122d0f4040feadad51618118d92243dc5f2b
Javier sanchez <javier.ant.sanchezc@gmail.com>
2020-12-03 09:40:04 -0500

    primera parte

---
+++ src/main/java/com/app/bar/appbar/AppbarApplication.java
@@ -0,0 +1,13 @@
+package com.app.bar.appbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppbarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/bar/appbar/AppbarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bar.appbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
